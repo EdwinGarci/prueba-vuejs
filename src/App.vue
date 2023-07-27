6f5b85b2d9de595fdea70920fddf2e2267654d57
Edwin Garcia <edwingarciatavara@gmail.com>
2023-07-27 16:47:50 -0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,89 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+<script setup>
+import { computed, ref } from 'vue';
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  const name = "Fidel";
+  const styleColor = "color: blue";
+  const activo = false;
+  const objColores = [{
+    name: "rojo",
+    description: "Rojo",
+    stock: 0
+  },
+  {
+    name: "azul",
+    description: "Azul",
+    stock: 1
+  },
+  {
+    name: "verde",
+    description: "Verde",
+    stock: 2
+  }];
+  const objColor = {
+    name: "amarillo",
+    description: "Amarillo",
   }
-}
+
+  const handleClick = (message) => {
+    console.log(message);
+  }
+
+  const counter = ref(0);
+
+  const increment = () => {
+    counter.value++;
+  }
+
+  const decrement = () => {
+    counter.value--;
+  }
+
+  const reset = () => {
+    counter.value = 0;
+  }
+
+  const classCounter = computed(() => {
+    if (counter.value === 0) {
+      return 'zero';
+    } if (counter.value > 0) {
+      return 'positive';
+    } else {
+      return 'negative';
+    }
+  })
 </script>
+
+<template>
+  <h1 :style="styleColor">Hola {{name.toUpperCase()}}</h1>
+  <h2 v-if="activo">Estoy activo</h2>
+  <h2 v-else>Estoy inactivo</h2>
+  
+  <ul>
+    <!-- <li v-for="(item, index) in arrayColores" :key="index">
+      {{ item }}
+    </li> -->
+    <li v-for="item in objColores" :key="item.name">
+      {{ item.name }} - {{ item.description }}
+    </li>
+    <li v-for="(value, propiedad) in objColor" :key="value">
+      {{ propiedad }} : {{ value }}
+    </li>
+    <template v-for="item in objColores" :key="item.name">
+      <li v-if="item.stock > 0">
+        {{ item.name }} - {{ item.description }}
+      </li>
+    </template>
+  </ul>
+
+  <button v-on:click="handleClick('Texto 1')">Activar 1</button>
+  <button @click="handleClick('Texto 2')">Activar 2</button>
+
+  <button @click="increment()">Aumentar</button>
+  <button @click="decrement()">Disminuir</button>
+  <button @click="reset()">Reiniciar</button>
+  <!-- <h2 :class="counter > 0 ? 'positive' : 'negative' ">{{ counter }}</h2> -->
+  <h2 :class="classCounter">{{ counter }}</h2>
+</template>
 
 <style>
 #app {
@@ -22,5 +93,20 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1 {
+  color: red;
+}
+
+.positive {
+  color: greenyellow;
+}
+
+.negative {
+  color: red;
+}
+
+.zero {
+  color: black;
 }
 </style>
